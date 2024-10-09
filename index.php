4450dad2887fec04bea4e145a546648526179aaa
juanpdpp <164434341+juanpdpp@users.noreply.github.com>
2024-10-09 19:41:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css">
+<title>Cidade</title>
+</head>
+<?php
+    $url = "http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/5092/days/15?token=3dba06a390c064c2a9c4ee21118a128c";
+    $curl = curl_init();
+
+    curl_setopt_array($curl, [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true
+    ]);
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $cidade = json_decode($response, true);
+    $InfoHoje = $cidade["data"][0];
+
+    $dawnImg = $InfoHoje["text_icon"]["icon"]["dawn"];
+    $morningImg = $InfoHoje["text_icon"]["icon"]["morning"];
+    $afternoonImg = $InfoHoje["text_icon"]["icon"]["afternoon"];
+    $nightImg = $InfoHoje["text_icon"]["icon"]["night"];
+
+?>
+<body>
+    <header class="container">
+        <h1>Previsão para Hoje <?=$InfoHoje["date_br"] ?> de <?= $cidade["name"]?> - <?= $cidade["state"]?></h1>
+    </header>
+    <br>
+    <body>
+    <h2 class="container4 alinhar"><?= $InfoHoje["text_icon"]["text"]["pt"]?></h2>
+    <br>
+    <br>
+    <div class="imagens">
+        <div>Madrugada<img src="/imagens/<?=$dawnImg?>.png" alt="dawn"></div>
+        <div> Manhã <img src="/imagens/<?=$morningImg?>.png" alt="morning"></div>
+        <div> Tarde <img src="/imagens/<?=$afternoonImg?>.png" alt="afternoon"></div>
+        <div> Noite <img src="/imagens/<?=$nightImg?>.png" alt="night"></div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="alinhar">
+        <div class="container2">Temperatura:</div>
+        <div class="container3">Minima: <?= $InfoHoje["temperature"]["min"] ?> C° <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
+</svg></div>
+        <div class="container3">Máxima: <?= $InfoHoje["temperature"]["max"] ?> C° <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
+</svg></div>
+        <div class="container3">Probabilidade de chuva: <?=$InfoHoje["rain"]["probability"] ?> % <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-drizzle" viewBox="0 0 16 16">
+  <path d="M4.158 12.025a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m-3.5 1.5a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 0 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m6 0a.5.5 0 0 1 .316.633l-.5 1.5a.5.5 0 1 1-.948-.316l.5-1.5a.5.5 0 0 1 .632-.317m.747-8.498a5.001 5.001 0 0 0-9.499-1.004A3.5 3.5 0 1 0 3.5 11H13a3 3 0 0 0 .405-5.973M8.5 2a4 4 0 0 1 3.976 3.555.5.5 0 0 0 .5.445H13a2 2 0 0 1 0 4H3.5a2.5 2.5 0 1 1 .605-4.926.5.5 0 0 0 .596-.329A4 4 0 0 1 8.5 2"/>
+</svg></div>
+        <div class="container3">Humidade: <?= $InfoHoje["humidity"]["min"] ?> % <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0q.164.544.371 1.038c.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8m.413 1.021A31 31 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10a5 5 0 0 0 10 0c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
+  <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87z"/>
+</svg></div>
+    </div>
+        <br>
+    <div class="alinhar">
+        <div class="container2">Velocidade vento:</div>
+        <div class="container3">Minima: <?= $InfoHoje["wind"]["velocity_min"] ?> km/h <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
+  <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5"/>
+</svg></div>
+        <div class="container3">Máxima: <?= $InfoHoje["wind"]["velocity_max"] ?> km/h <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
+  <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5"/>
+</svg></div>
+        <br>
+    </div>
+    <div class="alinhar">
+        <div class="container2">Sencação termica:</div>
+        <div class="container3">Minima: <?= $InfoHoje["thermal_sensation"]["min"] ?> C° <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
+</svg></div>
+        <div class="container3">Máxima: <?= $InfoHoje["thermal_sensation"]["max"] ?> C° <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5"/>
+</svg></div>
+    </div>
+    </body>
+</html>
